3571d58daa5a2143f468da9da9edc606ab2307ef
rian <rianerlangga03@gmail.com>
2020-06-24 22:42:40 +0700

    add part1

---
+++ part1/Animation Tutorial/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Animation Tutorial
+//
+//  Created by Rian Erlangga Saputra on 24/06/20.
+//  Copyright © 2020 rerlanggas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var easeInOutButton: UIButton!
+    @IBOutlet weak var easeOutButton: UIButton!
+    @IBOutlet weak var easeInButton: UIButton!
+    @IBOutlet weak var linearButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        startAnimationButton1()
+    }
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        resetAnimation()
+    }
+    
+    private func startAnimationButton1() {
+        UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+            self.linearButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn, animations: {
+            self.easeInButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: {
+            self.easeOutButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut, animations: {
+            self.easeInOutButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
+        }, completion: nil)
+    }
+    
+    private func resetAnimation() {
+        easeInOutButton.transform = CGAffineTransform.identity
+        easeOutButton.transform = CGAffineTransform.identity
+        easeInButton.transform = CGAffineTransform.identity
+        linearButton.transform = CGAffineTransform.identity
+    }
+}
+
