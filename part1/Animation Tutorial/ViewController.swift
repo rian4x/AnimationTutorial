05a15190413add1e95399bdd6bf704d5e20159c9
rian <rianerlangga03@gmail.com>
2020-10-06 10:42:35 +0700

    create animation identity

--- part1/Animation Tutorial/ViewController.swift
+++ part1/Animation Tutorial/ViewController.swift
@@ -30,25 +30,33 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
             self.linearButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
         }) { [weak self] _ in
-            self?.linearButton.transform = CGAffineTransform.identity
+            UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+                self?.linearButton.transform = CGAffineTransform.identity
+            })
         }
         
         UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn, animations: {
             self.easeInButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
         }) { [weak self] _ in
-            self?.easeInButton.transform = CGAffineTransform.identity
+            UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn, animations: {
+                self?.easeInButton.transform = CGAffineTransform.identity
+            })
         }
         
         UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: {
             self.easeOutButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
         }) { [weak self] _ in
-            self?.easeOutButton.transform = CGAffineTransform.identity
+            UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: {
+                self?.easeOutButton.transform = CGAffineTransform.identity
+            })
         }
         
         UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut, animations: {
             self.easeInOutButton.transform = CGAffineTransform.init(translationX: 200, y: 0)
         }) { [weak self] _ in
-            self?.easeInOutButton.transform = CGAffineTransform.identity
+            UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut, animations: {
+                self?.easeInOutButton.transform = CGAffineTransform.identity
+            })
         }
     }
     
